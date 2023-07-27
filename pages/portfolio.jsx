a721428e1206d3cc93f63abfd0b880b330915257
Yzabelle Go <ytgo@uwaterloo.ca>
2023-07-26 23:58:21 -0400

    portfolio layout 1

--- pages/portfolio.jsx
+++ pages/portfolio.jsx
@@ -2,11 +2,26 @@ import Link from "next/link";
 
 export default function Portfolio() {
   return (
-    <>
-      <h1>First Post</h1>
-      <h2>
-        <Link href="/">Back to home</Link>
-      </h2>
-    </>
+    <main>
+      <div>
+        <img
+          src="/images/placeholder.png"
+          style={{ width: "30vh", display: "inline-block" }}
+        ></img>
+        <div style={{ display: "inline-block" }}>
+          <h1>First Post</h1>
+          <h2>
+            <Link href="/">Back to home</Link>
+          </h2>
+        </div>
+      </div>
+      <style jsx>{`
+        main {
+          display: grid;
+          place-content: center;
+          max-height: 100vh;
+        }
+      `}</style>
+    </main>
   );
 }
