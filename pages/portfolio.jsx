ed9e5388ecb146f876bdafcaa805f515ff4b541a
Yzabelle Go <ytgo@uwaterloo.ca>
2023-07-29 00:54:46 -0400

    portfolio styling

--- pages/portfolio.jsx
+++ pages/portfolio.jsx
@@ -1,19 +1,51 @@
 import HomeButton from "../components/homeButton";
 import MenuButton from "../components/menuButton";
+import styles from "../styles/Portfolio.module.css";
 
 export default function Portfolio() {
   return (
-    <div style={{ display: "grid", placeContent: "center" }}>
+    <div>
       <HomeButton />
       <MenuButton />
-      {/* weirdge translation */}
-      <div style={{ transform: "translate(0, 50%)" }}>
-        <img
-          src="/images/placeholder.png"
-          style={{ width: "30vh", display: "inline-block" }}
-        ></img>
-        <div style={{ display: "inline-block" }}>
-          <h1>First Post</h1>
+      <div className={styles.container}>
+        <div>
+          <img
+            src="/images/placeholder.png"
+            style={{
+              width: "32vw",
+              display: "inline-block",
+              verticalAlign: "top",
+            }}
+          ></img>
+          <div
+            style={{
+              display: "inline-block",
+              minWidth: "30vw",
+              marginLeft: "6rem",
+            }}
+          >
+            <h1>Projects</h1>
+            <hr className={styles.hr} />
+            <div className={styles.project}>
+              <h2>uwexplore</h2>
+              <h3>web</h3>
+            </div>
+            <hr className={styles.hr} />
+            <div className={styles.project}>
+              <h2>uwexplore</h2>
+              <h3>web</h3>
+            </div>
+            <hr className={styles.hr} />
+            <div className={styles.project}>
+              <h2>uwexplore</h2>
+              <h3>web</h3>
+            </div>
+            <hr className={styles.hr} />
+            <div className={styles.project}>
+              <h2>uwexplore</h2>
+              <h3>web</h3>
+            </div>
+          </div>
         </div>
       </div>
     </div>
